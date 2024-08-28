ab96354e547b677174cad77b686703099bb21783
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2024-08-28 00:24:21 -0600

    Commit Inicial

---
+++ cart.js
@@ -0,0 +1,42 @@
+let productos = JSON.parse(localStorage.getItem('productos'));
+
+function drag(ev) {
+    ev.stopPropagation();
+    ev.target.style.cursor = 'grabbing'
+    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("nombre", ev.target.dataset.nombre);
+    ev.dataTransfer.setData("precio", ev.target.dataset.precio);
+}
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    const element = document.getElementById(data)
+    if (!element.draggable) {
+        return
+    }
+    element.style.cursor = 'grab'
+    const src = document.getElementById(data).firstElementChild.src
+    const img = document.createElement('img')
+    img.src = src
+    img.style.width = '45px'
+    img.style.margin = 0
+
+    let nuevoProducto = {
+        id: productos.length + 1,
+        nombre: ev.dataTransfer.getData('nombre'),
+        precio: ev.dataTransfer.getData('precio')
+    }
+
+    productos = [...productos, nuevoProducto]
+
+    localStorage.setItem('productos', JSON.stringify(productos))
+    contarProductos();
+    ev.target.appendChild(img);
+}
---
+++ scripts.js
@@ -0,0 +1,40 @@
+const spanLugar = document.getElementById('lugar')
+
+
+const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(`Latitud: ${latitude}, Longitud: ${longitude}`);
+    obtenerPais(latitude, longitude); // Llamar a la función para obtener el país
+}
+
+const error = () => {
+    console.log("No se pudo obtener la ubicación.");
+}
+
+const obtenerPais = (lat, lng) => {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=10&addressdetails=1`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            spanLugar.textContent = `${data.address.district}, ${data.address.city}, ${data.address.country}`
+            // console.log(data);
+        })
+        .catch(error => console.log("Error en la petición:", error));
+}
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+} else {
+    console.log("La geolocalización no es soportada por este navegador.");
+}
+
+const contarProductos = () => {
+    let productos = JSON.parse(localStorage.getItem('productos'))
+    let cantidad = productos.length
+    document.getElementById('contador').innerText = cantidad
+}
+
+contarProductos();
